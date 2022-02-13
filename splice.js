7f78f9727eb174646ca12c8e8798c77b648ba618
zannatreya <zannatulbintabahar@yahoo.com>
2022-02-14 02:27:22 +0600

    splice done

---
+++ splice.js
@@ -0,0 +1,8 @@
+const months = ['jan', 'feb', 'april', 'may'];
+months.splice(2, 0, 'mar');
+console.log(months);
+//['jan', 'feb','mar', 'april', 'may']
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruits);
